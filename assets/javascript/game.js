72b22fa51f1505e023f8ba66c9befc0fe380436a
Sam <sam@belairmanufacturing.onmicrosoft.com>
2017-09-26 14:46:15 -0500

    break up words into seperate divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,13 @@ var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 
 function createButtons() {
     for(i = 0; i < alpha.length; i++){
+        if(i == 13){
+            newLine = document.createElement("br");
+            document.getElementById("guessed").append(newLine);
+        }
         newBtn = document.createElement("div");
-        newBtn.innerHTML = alpha[i];
+        newBtn.classList.add("letter-btn");
+        newBtn.innerHTML = "<p>" + alpha[i] + "</p>";
         document.getElementById("guessed").append(newBtn);
     }
 }
@@ -13,25 +18,59 @@ function pickWord() {
     return songs[Math.floor(Math.random() * songs.length)]
 }
 
-function setBoard(word) {
-    for(i = 0; i < word.length; i++) {
-        ltrDiv = document.createElement("div");
-        ltrDiv.classList.add("letter-box");
-        ltrDiv.innerHTML = "o";
-        if(word[i] == " ") {
-            ltrDiv.classList.add("space");
-        };
+function setBoard(answer) {
+    answerArray = answer.split(" ");
 
-        if(word[i + 1] == "'") {
-            ltrDiv.setAttribute("style", "text-align: right; color: black;");
-            ltrDiv.innerHTML = " '";
-            i++;
-        }
-        else {
+    for(wrd = 0; wrd < answerArray.length; wrd++) {
+
+        wrdDiv = document.createElement("div");
+        wrdDiv.classList.add("word");
+        wrd_id = "word" + wrd;
+        wrdDiv.id = wrd_id;
+        document.getElementById("letters").append(wrdDiv);
+        console.log()
+
+        for(ltr = 0; ltr < answerArray[wrd].length; ltr++) {
             
-        }
+            ltrDiv = document.createElement("div");
 
-        document.getElementById("letters").append(ltrDiv);
+            ltrDiv.classList.add("letter");
+
+            ltrDiv.innerHTML = "o";
+
+            if(wrd[ltr] == " ") {
+
+                ltrDiv.classList.add("space");
+                };
+
+            
+            if(wrd[ltr + 1] == "'") {
+
+                ltrDiv.setAttribute("style", "text-align: right; color: black;");
+                ltrDiv.innerHTML = " '";
+
+                ltr++;
+            };
+           
+            document.getElementById(wrd_id).append(ltrDiv);
+        };
+    //     ltrDiv = document.createElement("div");
+    //     ltrDiv.classList.add("letter-box");
+    //     ltrDiv.innerHTML = "o";
+    //     if(word[i] == " ") {
+    //         ltrDiv.classList.add("space");
+    //     };
+
+    //     if(word[i + 1] == "'") {
+    //         ltrDiv.setAttribute("style", "text-align: right; color: black;");
+    //         ltrDiv.innerHTML = " '";
+    //         i++;
+    //     }
+    //     else {
+            
+    //     }
+
+    //     document.getElementById("letters").append(ltrDiv);
 
     
     }
