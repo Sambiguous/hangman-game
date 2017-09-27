43821a7244ab72a25b3d13503d6affddd854a330
Sam <sjdefrank91@gmail.com>
2017-09-26 22:59:12 -0500

    complete shadow function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 var songs = ["For Your Life", "The Song Remains the Same", "In the Evening", "Good Times Bad Times", "Ten Years Gone", "The Wanton Song", "Your Time is Gonna Come", "All My Love", "Houses of the Holy", "Thank You", "Custard Pie", "Since I've Been Loving You"];
 var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var round_array = [];
+var shadow = [];
+var test = "Since I've Been Loving You";
+
+
 
 function createButtons() {
     for(i = 0; i < alpha.length; i++){
@@ -18,69 +23,70 @@ function pickWord() {
     return songs[Math.floor(Math.random() * songs.length)]
 }
 
-function setBoard(answer) {
-    answerArray = answer.split(" ");
+function createShadowArray(template) {  //creates shadow of current round's answer. blanks are mapped to this shadow
+    shadow = [];
+    for(i = 0; i < template.length; i++) {
+        shadow[i] = [];
+        for(k = 0; k < template[i].length; k++) {
+            if(alpha.includes(template[i][k].toUpperCase())) {
+                shadow[i][k] = 0;
+            }
+            else {
+                shadow[i][k] = template[i][k];
+            }
+        }
 
-    for(wrd = 0; wrd < answerArray.length; wrd++) {
+    }
+    
+    return shadow;
+}
 
+function setBoard(word) {   // populates the '#letters' div with blanks to match the round's answer
+    //split 'word' parameter into an array
+    round_array = word.split(" ");
+    shadow_array = createShadowArray(round_array);
+    //create one div per element in the 'round_array' array
+    for(wrd = 0; wrd < round_array.length; wrd++) {
+        //create new div
         wrdDiv = document.createElement("div");
+        //assign class 'word'
         wrdDiv.classList.add("word");
+        //assign id 'word' plus number
         wrd_id = "word" + wrd;
         wrdDiv.id = wrd_id;
+        //append new div to 'letters' div
         document.getElementById("letters").append(wrdDiv);
-        console.log()
-
-        for(ltr = 0; ltr < answerArray[wrd].length; ltr++) {
-            
+        //create one div per character in each word
+        for(ltr = 0; ltr < round_array[wrd].length; ltr++) {
+            //create div
             ltrDiv = document.createElement("div");
-
+            //assign 'letter' class
             ltrDiv.classList.add("letter");
-
-            ltrDiv.innerHTML = "o";
-
-            if(wrd[ltr] == " ") {
-
-                ltrDiv.classList.add("space");
-                };
+            //set initial html
+            ltrDiv.innerHTML = shadow_array[wrd][ltr];
 
             
-            if(wrd[ltr + 1] == "'") {
-
-                ltrDiv.setAttribute("style", "text-align: right; color: black;");
-                ltrDiv.innerHTML = " '";
-
-                ltr++;
+            if( !(alpha.includes(round_array[wrd][ltr].toUpperCase())) ) {
+                ltrDiv.classList.add("punc");
+                ltrDiv.innerHTML = round_array[wrd][ltr];
             };
            
             document.getElementById(wrd_id).append(ltrDiv);
         };
-    //     ltrDiv = document.createElement("div");
-    //     ltrDiv.classList.add("letter-box");
-    //     ltrDiv.innerHTML = "o";
-    //     if(word[i] == " ") {
-    //         ltrDiv.classList.add("space");
-    //     };
 
-    //     if(word[i + 1] == "'") {
-    //         ltrDiv.setAttribute("style", "text-align: right; color: black;");
-    //         ltrDiv.innerHTML = " '";
-    //         i++;
-    //     }
-    //     else {
-            
-    //     }
-
-    //     document.getElementById("letters").append(ltrDiv);
-
-    
     }
 }
+
+
 
 createButtons();
 
 word = pickWord();
 
 setBoard(word);
+
+console.log(shadow_array);
+
 
 
 
